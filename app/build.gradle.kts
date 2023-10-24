d6e3e10ee9e3d55acb1e0ac2fe7684f776d29188
Yeonji <yeonm217@gmail.com>
2023-10-24 21:25:42 +0900

    ㅇ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.blankviewactivity"
-    compileSdk = 33
+    compileSdk = 34
 
     viewBinding{
         enable = true
--- app/src/main/java/com/example/blankviewactivity/MainActivity.kt
+++ app/src/main/java/com/example/blankviewactivity/MainActivity.kt
@@ -11,5 +11,7 @@ class MainActivity : AppCompatActivity() {//made by P1su
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.textView.text = num.toString()
     }
 }
