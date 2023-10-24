c3f7c89e00504e1144e778807bd0772fbb57d031
P1su <pissssssu@naver.com>
2023-10-24 17:48:08 +0900

    viewBinding and val num

--- app/src/main/java/com/example/blankviewactivity/MainActivity.kt
+++ app/src/main/java/com/example/blankviewactivity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.blankviewactivity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {//made by P1su
+    val num : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
